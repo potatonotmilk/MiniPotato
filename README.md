6b4d6d551bf7317ed8e57e969aa5ea86a1106006
ExtEOi <e.of.i.120@gmail.com>
2024-05-20 03:08:44 +0900

    Init

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# MiniPotato
+# MiniPotato Bot
+
+
+note にて解説しているので、そちらをご参照ください！
+
+解説ページはこちら　[https://note.com/exteoi/n/n0ea64e258797](https://note.com/exteoi/n/n0ea64e258797)
---
+++ main.mjs
@@ -0,0 +1,74 @@
+import fs from "fs";
+import path from "path";
+import express from "express";
+import { Client, Collection, Events, GatewayIntentBits, ActivityType, EmbedBuilder } from "discord.js";
+import CommandsRegister from "./regist-commands.mjs";
+import Notification from "./models/notification.mjs";
+import Sequelize from "sequelize";
+
+const app = express();
+app.listen(3000);
+app.get('/', function(req, res) {
+  res.send('<a href="https://note.com/exteoi/n/n0ea64e258797</a> に解説があります。');
+})
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
+});
+
+client.commands = new Collection();
+
+const categoryFoldersPath = path.join(process.cwd(), "commands");
+const commandFolders = fs.readdirSync(categoryFoldersPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(categoryFoldersPath, folder);
+  const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".mjs"));
+  
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    import(filePath).then((module) => {
+      client.commands.set(module.data.name, module);
+    });
+  }
+}
+
+const handlers = new Map();
+
+const handlersPath = path.join(process.cwd(), "handlers");
+const handlerFiles = fs.readdirSync(handlersPath).filter((file) => file.endsWith(".mjs"));
+
+for (const file of handlerFiles) {
+  const filePath = path.join(handlersPath, file);
+  import(filePath).then((module) => {
+    handlers.set(file.slice(0, -4), module);
+  });
+}
+
+client.on("interactionCreate", async (interaction) => {
+  await handlers.get("interactionCreate").default(interaction);
+});
+
+client.on("voiceStateUpdate", async (oldState, newState) => {
+  await handlers.get("voiceStateUpdate").default(oldState, newState);
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.id == client.user.id || message.author.bot) return;
+  await handlers.get("messageCreate").default(message);
+});
+
+client.on("ready", async () => {
+  await client.user.setActivity('🥔', { type: ActivityType.Custom, state: "🥔を栽培中" });
+  console.log(`${client.user.tag} がログインしました！`);
+});
+
+Notification.sync({ alter: true });
+
+CommandsRegister();
+client.login(process.env.TOKEN);
